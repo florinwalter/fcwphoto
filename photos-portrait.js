e776efbde9ad64c0e3a18e0a1576debdbdc7397e
flcello <57724549+flcello@users.noreply.github.com>
2026-04-26 18:45:54 +0200

    added new photo

--- photos-portrait.js
+++ photos-portrait.js
@@ -1,13 +1,14 @@
 // Portrait photos — shared by portrait.html and portrait-de.html
 // Only 'src' is required; 'title' is optional.
 const PHOTOS = [
-  { src: 'https://www.flickr.com/photo_download.gne?id=55227970469&secret=0d0d8f5778&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55227970469' },
+  { src: 'https://www.flickr.com/photo_download.gne?id=55230367874&secret=c5243467a3&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230367874'},
   { src: 'https://www.flickr.com/photo_download.gne?id=55230276268&secret=6c398b4c65&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230276268'},
   { src: 'https://www.flickr.com/photo_download.gne?id=55229210742&secret=c27e619da6&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55229210742'},
   { src: 'https://www.flickr.com/photo_download.gne?id=55230273923&secret=eec83617be&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230273923'},
   { src: 'https://www.flickr.com/photo_download.gne?id=55230529675&secret=a89087a31d&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230529675'},
   { src: 'https://www.flickr.com/photo_download.gne?id=55230366679&secret=8f66e46b07&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230366679'},
   { src: 'https://www.flickr.com/photo_download.gne?id=55230529675&secret=665982a23e&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230529675'},
-  { src: 'https://www.flickr.com/photo_download.gne?id=55230367874&secret=c5243467a3&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55230367874'},
+  { src: 'https://www.flickr.com/photo_download.gne?id=55227970469&secret=0d0d8f5778&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55227970469' },
+  { src: 'https://www.flickr.com/photo_download.gne?id=55231634986&secret=84f75e4e34&size=l&source=photoPageEngagement', flickrURL: 'https://www.flickr.com/photos/flello/55231634986'},
 
 ];
